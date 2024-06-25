b54323b3b1c68f60069caa32e7bc416525aeb3da
TaniaGu <tingxuegu@outlook.com>
2024-06-25 15:58:50 -0400

    Update Model.jl

--- Model.jl
+++ Model.jl
@@ -1,1 +1,15 @@
-# 
+# create the MILP model 
+
+#store the service locations data in the csv file 
+
+
+#store the service charging locations data in the csv file 
+
+# store the charger type data ion the csv file 
+
+
+###
+#case studies
+###
+
+#low EV range ()
